a58d99dcd36891978e9f44c86e5b5b75f9b103c3
Vic7ord1st <okoroafor.victor@gmail.com>
2019-06-03 22:32:17 +0100

    Changed the use of a stateful widget to a stateful builder to remove boiler plate code

--- lib/carrousel.dart
+++ lib/carrousel.dart
@@ -3,29 +3,28 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'popUp.dart';
 
 
-class Carrousel extends StatefulWidget{
+class Carrousel extends StatelessWidget{
   @override
-  _CarrouselState createState()=> _CarrouselState();
-}
-
-class _CarrouselState extends State <Carrousel>{
-  @override
+  
   Widget build(BuildContext context){
-    return CarouselSlider(
-      height: 200,
-      items: <Widget>[
-        PopUp('New Age Saloon','4534 Lakeshore drive'),
-        PopUp('Magnificent looks','7, Thors Brine'),
-        PopUp('Gregs Beef','Old Guards Boulevard'),
-        PopUp('Jennys Shop','Allington Avenue')
-      ],
-      autoPlay: false,
-      enableInfiniteScroll: false,
-      onPageChanged: (index) {
+    return StatefulBuilder(
+      builder: (ctx, StateSetter setState) => 
+        CarouselSlider(
+          height: 200,
+          items: <Widget>[
+             PopUp('New Age Saloon','4534 Lakeshore drive'),
+             PopUp('Magnificent looks','7, Thors Brine'),
+             PopUp('Gregs Beef','Old Guards Boulevard'),
+             PopUp('Jennys Shop','Allington Avenue')
+          ],
+          autoPlay: false,
+          enableInfiniteScroll: false,
+          onPageChanged: (index) {
          setState(() {
           }
         );
       },
+    )
     );
   }
 }
